85b8f92d62dd8cd3342ac26d3c74362d3e32fc3c
Harish-85 <66102112+Harish-85@users.noreply.github.com>
2024-12-10 14:52:24 +0530

    removed other projects

--- src/views/GameProjects.vue
+++ src/views/GameProjects.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>GAME PROJECTS</h1>
+    <div style="margin-bottom: -50px">
+      <h1>GAME PROJECTS</h1>
+    </div>
 
     <div style="margin-bottom: 30px;">
       These are some of the games I've worked on in the past.
@@ -11,14 +13,21 @@
     <div style="margin-top: 20px;">
       There is more to see on <a target="_blank" href="https://unknown-coder.itch.io/">itch.io</a>
     </div>
+
+    <h1>MINI PROJECTS</h1>
+
+    <div style="margin-bottom: 30px;">
+      Here are some mechanics and small projects I've worked on that are not complete games.
+    </div>
+    <ProjectsList v-bind:projects="otherProjects" />
+
   </div>
 </template>
-
 <script lang="ts">
 import Vue from "vue";
 import ProjectsList from "@/components/ProjectsList.vue";
 import gameProjectsData from "@/data/GameProjectsData.ts";
-
+import otherProjectsData from "@/data/OtherProjectsData.ts";
 export default Vue.extend({
   name: "GameProjects",
   components: {
@@ -27,7 +36,11 @@ export default Vue.extend({
   data: function () {
     return {
       projects: gameProjectsData,
+      otherProjects: otherProjectsData,
     };
   },
 });
+
 </script>
+
+
